2a257fd44794d47fecdde2286cbce3552f0609af
Sang HT <huynhtsang2004@gmail.com>
2024-10-25 13:53:33 +0700

    Update Sign In and Layout

---
+++ app/Http/Controllers/Admin/Users/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Users;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class LoginController extends Controller
+{
+    public function index(){
+        return view("admin.users.login",[
+            'title' => 'Dang nhap'
+        ]);
+    }
+
+    //Request de lay thong tin tu form
+    public function store(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ], [
+            'email.required' => 'Vui lòng nhập địa chỉ email.',
+            'email.email' => 'Địa chỉ email không đúng định dạng.',
+            'password.required' => 'Vui lòng nhập mật khẩu.',
+        ]);
+    
+        // Xử lý đăng nhập sau khi xác thực thành công
+
+        if(Auth::attempt([      //Auth::attempt nhận một mảng thông tin
+            'email'=> $request->input('email'),
+            'password' => $request->input('password')]))
+        {
+            return redirect() -> route('admin');
+        }
+        // Nếu đăng nhập không thành công, trả về trang trước đó với thông báo lỗi
+        return redirect()->back()->withErrors([
+            'login' => 'Thông tin đăng nhập không chính xác.',
+        ])->withInput(); // Giữ lại giá trị input của người dùng
+    }
+}
