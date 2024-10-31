22766d81b95142d1828a881633d0079e455fbdf7
Sang HT <huynhtsang2004@gmail.com>
2024-10-31 11:34:01 +0700

    123

--- app/Http/Controllers/frontend/HomeController.php
+++ app/Http/Controllers/frontend/HomeController.php
@@ -4,12 +4,10 @@ namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
 use App\Http\Services\Menu\MenuService;
-<<<<<<< HEAD
 
-=======
 use App\Http\Services\Slider\SliderService;
 use Illuminate\Http\Request;
->>>>>>> 0c763c3c91f51227b336369c7225d46cb773ca36
+
 
 class HomeController extends Controller
 {
@@ -27,7 +25,6 @@ class HomeController extends Controller
             'title' => 'Shop-SP',
            'sliders' => $this->slider->show(),
            'menus'=>$this->menu->show()
->>>>>>> 0c763c3c91f51227b336369c7225d46cb773ca36
         ]);
     }
 }
